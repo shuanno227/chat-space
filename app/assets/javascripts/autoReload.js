956c1d99b87ef5cd711d01a4e5b2f37651e8a5a8
shuanno227 <shuanno227@gmail.com>
2020-06-30 11:00:47 +0900

    fix "add AutomaticUpload"

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -43,15 +43,11 @@ $(function(){
 
   
   let reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     let last_message_id = $('.chatBox:last').data("message-id");
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
