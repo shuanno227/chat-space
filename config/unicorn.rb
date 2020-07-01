b81e40c23711eaac00e9d3b2acdc36f8753e5c74
shuanno227 <shuanno227@gmail.com>
2020-07-01 12:23:59 +0900

    create capfile

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,8 +5,12 @@ app_path = File.expand_path('../../', __FILE__)
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+working_directory "#{app_path}/current"
 
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
